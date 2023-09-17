1b01f42cd59037cfe0972f9eace19c7b854742db
santiago sanchez <ivsasafo30@hotmail.com>
2023-09-17 15:23:24 -0500

    calculando promedios de edad

--- RegistrosMedicos.py
+++ RegistrosMedicos.py
@@ -1,17 +1,42 @@
 from datasets import load_dataset
+import pandas as pd
 import numpy as np
 
 def cargar_dataset():
     dataset = load_dataset("mstz/heart_failure")
-    return dataset
+    data = dataset['train']
+    return data
     
 def promedio_edad(dataset)->float:
-    data=dataset['train']
-    edades=data['age']
+    edades=dataset['age']
     edades_np=np.array(edades)
     edad_promedio=np.mean(edades_np)
-    print(edad_promedio)
     return edad_promedio
 
-promedio_edad(cargar_dataset())
+def convertir_dataset(dataset)->pd.DataFrame:
+    df=pd.DataFrame(dataset)
+    return df
+
+def separar_dtframe (df: pd.DataFrame)->pd.DataFrame:
+    difuntos=df[df['is_dead']==1]
+    aun_vivos=df[df['is_dead']!=1]
+    return difuntos,aun_vivos
+
+def imprimir_promedios(difuntos,vivos:pd.DataFrame):
+    promedio_dif=np.mean(difuntos['age'])
+    promedio_viv=np.mean(vivos['age'])
+    print("edad promedio de muerte: " +str( promedio_dif))
+    print("edad promedio de vivos: " +str( promedio_viv))
+    
+
+
+dataset=cargar_dataset()
+df=convertir_dataset(dataset)
+difuntos,aunvivos=separar_dtframe(df)
+imprimir_promedios(difuntos,aunvivos)
+    
+    
+    
+
+  
 
