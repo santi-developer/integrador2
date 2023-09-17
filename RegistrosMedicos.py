dfe59b7fb4d3dfbef80d8c0961469a5bff93deeb
santiago sanchez <ivsasafo30@hotmail.com>
2023-09-17 14:01:12 -0500

    haciendo uso del POO

--- RegistrosMedicos.py
+++ RegistrosMedicos.py
@@ -1,16 +1,17 @@
 from datasets import load_dataset
 import numpy as np
 
-dataset = load_dataset("mstz/heart_failure")
+def cargar_dataset():
+    dataset = load_dataset("mstz/heart_failure")
+    return dataset
+    
+def promedio_edad(dataset)->float:
+    data=dataset['train']
+    edades=data['age']
+    edades_np=np.array(edades)
+    edad_promedio=np.mean(edades_np)
+    print(edad_promedio)
+    return edad_promedio
 
-data=dataset['train']
-
-edades=data['age']
-
-edades_np=np.array(edades)
-
-edad_promedio=int(np.mean(edades_np))
-
-print(edad_promedio)
-
+promedio_edad(cargar_dataset())
 
